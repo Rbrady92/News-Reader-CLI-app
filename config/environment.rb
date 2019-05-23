d0e7aa11d8b85dc6eaffae3aab6850a318418758
Rbrady92 <37550582+Rbrady92@users.noreply.github.com>
2019-05-23 18:15:41 -0400

    Update environment

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,9 @@
 require 'nokogiri'
 require 'open-uri'
-require 'require_all'
+#require 'require_all'
 
 #require_all 'lib'
 
-require_relative "../lib/categories"
-require_relative "../lib/cli"
-require_relative "../lib/messages"
+require_relative "../lib/articles"
 require_relative "../lib/scraper"
-require_relative "../lib/site_list"
+require_relative "../lib/cli"
